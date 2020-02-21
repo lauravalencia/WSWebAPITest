403c14d35a7598cb9bf709a0ad4e1893ab06ecc9
Laura Valencia <laura.valencia@contractors.accruent.com>
2020-02-21 15:46:14 -0500

    Add project files.

---
+++ WSWebAPITest/Services/ShoppingService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSWebAPITest.Entities;
+using WSWebAPITest.Services.Interfaces;
+
+namespace WSWebAPITest.Services
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        public ShoppingItem Add(ShoppingItem newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ShoppingItem> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingItem GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
